c91a9a2dd1686ac5a370a4e6ea721c34144ac639
SubDragonzj <SubDragonzj@139.com>
2016-05-07 19:51:05 +0800

    dior: Enable native tap to wake
    Change-Id: I2dd5ef88a4cfc606637695ced45a18dc8598aa32

--- drivers/input/touchscreen/atmel/plug.h
+++ drivers/input/touchscreen/atmel/plug.h
@@ -1,7 +1,6 @@
 /*
-* Copyright (C) 2015 XiaoMi, Inc.All rights reserved.
+* Copyright (C) 2015 XiaoMi, Inc. All rights reserved.
 */
-
 #ifndef __LINUX_ATMEL_MXT_PLUG
 #define __LINUX_ATMEL_MXT_PLUG
 
